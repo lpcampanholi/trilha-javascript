1c99a509ce8e13b09685439202659d2547b5a92f
Luís Campanholi <lpcampanholi@gmail.com>
2023-09-21 10:04:01 -0300

    Set e métodos avançados de Array

---
+++ Arrays/copiando-array.js
@@ -0,0 +1,7 @@
+const notas = [7, 7, 8, 9];
+
+// spread operator:
+const novasNotas = [...notas, 10];
+
+console.log(`As novas notas são ${novasNotas}`);
+console.log(`As notas originais são ${notas}`);
